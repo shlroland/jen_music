b69160731d1b8b6961f4b86abf46716ac45785fc
shlroland <shlroland1995@gmail.com>
2021-05-27 00:05:35 +0800

    添加bottomnavigator

---
+++ lib/navigator/bottom_navigator.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:jen_music/utils/color.dart';
+
+class BottomNavigator extends StatefulWidget {
+  @override
+  _BottomNavigatorState createState() => _BottomNavigatorState();
+}
+
+class _BottomNavigatorState extends State<BottomNavigator> {
+  int _currentIndex = 0;
+  final _defaultColor = Colors.grey;
+  final _activeColor = primary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => _onJumpTo(index),
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: _activeColor,
+        items: [
+          _bottomItem('首页', Icons.umbrella),
+          _bottomItem('搜索', Icons.search_outlined),
+          _bottomItem('歌单', Icons.headset_rounded),
+          _bottomItem('我的', Icons.person_outlined),
+        ],
+      ),
+    );
+  }
+
+  _bottomItem(
+    String title,
+    IconData icon,
+  ) {
+    return BottomNavigationBarItem(
+        icon: Icon(icon),
+        activeIcon: Icon(icon, color: _activeColor),
+        label: title);
+  }
+
+  void _onJumpTo(int index) {
+    setState(() {
+      //控制选中第一个tab
+      _currentIndex = index;
+    });
+  }
+}
---
+++ lib/utils/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+/// 主色调
+const MaterialColor primary = const MaterialColor(0xffff2d55, <int,Color>{
+  50: const Color(0xffffe4e9),
+
+});
