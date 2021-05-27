179c65d0ec13eec7baee6164d86a0b0546432290
shlroland <shlroland1995@gmail.com>
2021-05-27 22:43:52 +0800

    feat: 添加了页面demo

--- lib/navigator/bottom_navigator.dart
+++ lib/navigator/bottom_navigator.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:jen_music/pages/album_page.dart';
+import 'package:jen_music/pages/category_page.dart';
+import 'package:jen_music/pages/home_page.dart';
+import 'package:jen_music/pages/profile_page.dart';
+import 'package:jen_music/pages/search_page.dart';
 import 'package:jen_music/utils/color.dart';
 
 class BottomNavigator extends StatefulWidget {
@@ -10,10 +15,25 @@ class _BottomNavigatorState extends State<BottomNavigator> {
   int _currentIndex = 0;
   final _defaultColor = Colors.grey;
   final _activeColor = primary;
+  static int initialPage = 0;
+  final PageController _controller = PageController(initialPage: initialPage);
+  List<Widget> _pages;
 
   @override
   Widget build(BuildContext context) {
+    _pages = [
+      HomePage(),
+      SearchPage(),
+      CategoryPage(),
+      AlbumPage(),
+      ProfilePage(),
+    ];
     return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: _pages,
+        onPageChanged: (index) => _onJumpTo(index, pageChange: true),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) => _onJumpTo(index),
         currentIndex: _currentIndex,
@@ -22,6 +42,7 @@ class _BottomNavigatorState extends State<BottomNavigator> {
         items: [
           _bottomItem('首页', Icons.home),
           _bottomItem('搜索', Icons.search_outlined),
+          _bottomItem('分类', Icons.graphic_eq_outlined),
           _bottomItem('歌单', Icons.whatshot_outlined),
           _bottomItem('我的', Icons.person_outlined),
         ],
@@ -42,7 +63,7 @@ class _BottomNavigatorState extends State<BottomNavigator> {
         label: title);
   }
 
-  void _onJumpTo(int index) {
+  void _onJumpTo(int index, {pageChange = false}) {
     setState(() {
       //控制选中第一个tab
       _currentIndex = index;
---
+++ lib/pages/album_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlbumPage extends StatefulWidget {
+  @override
+  _AlbumPageState createState() => _AlbumPageState();
+}
+
+class _AlbumPageState extends State<AlbumPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('歌单页面')
+    );
+  }
+}
